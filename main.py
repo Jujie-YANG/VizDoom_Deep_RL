cebf2c570e1d298a3a585e12a4137fddf4efa105
Jujie-YANG <yangjujie3619@outlook.com>
2022-07-10 20:15:02 +0100

    new python file

---
+++ main.py
@@ -0,0 +1,8 @@
+# Import vizdoom for game env
+from vizdoom import * 
+# Import random for action sampling
+import random
+# Import time for sleeping
+import time 
+# Import numpy for identity matrix
+import numpy as np
